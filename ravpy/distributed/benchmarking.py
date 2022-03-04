33fd20eb6688e8dd399dcdd20c7c1e5611c30375
Anirudh Menon <senseianirudh28@gmail.com>
2022-03-04 16:17:14 +0530

    fixed benchmarking script

--- ravpy/distributed/benchmarking.py
+++ ravpy/distributed/benchmarking.py
@@ -1,35 +1,36 @@
 import json
 import time
 
-from ravop import compute_locally, functions
+from ravop import functions
 
 from ..config import SOCKET_SERVER_URL, BENCHMARK_FILE_NAME
-from ..utils import download_file, get_key
-
+from ..utils import download_file, get_key, setTimeout
+from ..globals import g
+from .compute import compute_locally_bm
+from .evaluate import waitInterval
 
 def benchmark():
+    client = g.client
+    initialTimeout = g.initialTimeout
+
     # Receive benchmarking ops
     download_file(url="{}/ravenjs/get/benchmark/".format(SOCKET_SERVER_URL), file_name=BENCHMARK_FILE_NAME)
 
     # Load benchmark file and execute ops
     with open(BENCHMARK_FILE_NAME, "rb") as f:
         benchmark_ops = json.load(f)
-
         benchmark_results = {}
 
-        for index, benchmark_op in enumerate(benchmark_ops):
-            # print(benchmark_op)
+        for benchmark_op in benchmark_ops:
+            print("BM OP inside enumerate: ",benchmark_op)
             operator = get_key(benchmark_op['operator'], functions)
             t1 = time.time()
-            print(compute_locally(*benchmark_op['values'], op_type=benchmark_op['op_type'], operator=operator))
+            print(compute_locally_bm(*benchmark_op['values'], op_type=benchmark_op['op_type'], operator=operator))
             t2 = time.time()
             benchmark_results[benchmark_op["operator"]] = t2 - t1
 
-            if index == 1:
-                break
-
-    print(benchmark_results)
-
+    print("\nEmitting Benchmark Results...")
+    client.emit("benchmark_callback", data=json.dumps(benchmark_results), namespace="/client")
+    client.sleep(1)
+    setTimeout(waitInterval, initialTimeout)
     return benchmark_results
-
-    # client.emit("benchmark_callback", data=json.dumps(benchmark_results), namespace="/client")
