bf64c8bb425c537f228294fae27cb0aecd419224
Unnikrishnan Menon <menon.uk1998@gmail.com>
2022-04-05 14:48:56 +0530

    ftp initialize function added

--- ravpy/distributed/benchmarking.py
+++ ravpy/distributed/benchmarking.py
@@ -1,13 +1,28 @@
 import json
 import time
+import ast
 
 from ravop import functions
 
 from ..config import SOCKET_SERVER_URL, BENCHMARK_FILE_NAME
-from ..utils import download_file, get_key, setTimeout
+from ..utils import download_file, get_key, setTimeout, get_ftp_credentials
 from ..globals import g
 from .compute import compute_locally_bm
 from .evaluate import waitInterval
+from ..ftp import check_credentials
+from ..ftp import get_client as get_ftp_client
+
+
+def initialize():
+    time.sleep(5)
+    creds = ast.literal_eval(get_ftp_credentials(g.cid)['ftp_credentials'])
+    print("Ftp credentials: ", creds)
+    time.sleep(2)
+
+    g.ftp_client = get_ftp_client(creds['username'], creds['password'])
+    print("Check creds:", check_credentials(creds['username'], creds['password']))
+    g.ftp_client.list_server_files()
+
 
 def benchmark():
     client = g.client
@@ -22,7 +37,7 @@ def benchmark():
         benchmark_results = {}
 
         for benchmark_op in benchmark_ops:
-            print("BM OP inside enumerate: ",benchmark_op)
+            # print("BM OP inside enumerate: ",benchmark_op)
             operator = get_key(benchmark_op['operator'], functions)
             t1 = time.time()
             print(compute_locally_bm(*benchmark_op['values'], op_type=benchmark_op['op_type'], operator=operator))
@@ -33,4 +48,5 @@ def benchmark():
     client.emit("benchmark_callback", data=json.dumps(benchmark_results), namespace="/client")
     client.sleep(1)
     setTimeout(waitInterval, initialTimeout)
+    initialize()
     return benchmark_results
